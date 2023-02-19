b156a35580734b0414e0888c954b26007f9a4bdc
Marcello Antony <marcello.antonymr@gmail.com>
2023-02-19 11:14:01 -0300

    API+Alterações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'OtherPic'
+  title = 'OtherPic';
+  photos =[
+    {
+      url:"https://www.petz.com.br/blog/wp-content/uploads/2022/01/curiosidades-sobre-leao.jpg",
+      description:"Leão selvagem"
+    },
+    {
+      url:"https://www.petz.com.br/blog/wp-content/uploads/2022/01/curiosidades-sobre-leao2-1280x720.jpg",
+      description:"Leão com fundo preto"
+    }
+  ]
 }
